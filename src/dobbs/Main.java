2b5826f312212d9333667e0c860b90791289139d
stevedobbs2 <83850110+stevedobbs2@users.noreply.github.com>
2021-05-12 19:15:46 -0400

    Update Main.java
    Edit to add clarification

--- src/dobbs/Main.java
+++ src/dobbs/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is only a test");
+	System.out.println("Hello! I can see it's a test.");
     }
 }
